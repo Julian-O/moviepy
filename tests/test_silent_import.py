ee6011afeed6ec7e5eee8fe0575d50f48b3ab210
Julian-O <github@somethinkodd.com>
2019-07-17 13:27:18 +1000

    Skip test if pygame not installed.
    The automated tests don't install pygame.

--- tests/test_silent_import.py
+++ tests/test_silent_import.py
@@ -6,7 +6,7 @@ to be the first to run the import."""
 
 try:
     from io import StringIO
-except:
+except ImportError:
     # Python 2.
     from cStringIO import StringIO
 import sys
@@ -15,22 +15,28 @@ import pytest
 
 
 def test_issue_985():
-
     old_stdout = sys.stdout
     sys.stdout = mystdout = StringIO()
 
     # Import a package that depends on pygame.
 
-    # Either of these will do. Both is overkill.
-    import moviepy.audio.io.preview
-    # import moviepy.video.io.preview
+    try:
 
-    # Not tested: Importing moviepy.video.tools.tracking and calling
-    # manual_tracking() which does an import.
+        # Either of these will do. Both is overkill.
+        import moviepy.audio.io.preview
+        # import moviepy.video.io.preview
+
+        # Not tested: Importing moviepy.video.tools.tracking and calling
+        # manual_tracking() which does an import.
+    except ImportError:
+        # Pygame and Pygame silent aren't both installed
+        # (which is how the automated tests run).
+        pytest.skip("Optional pygame is not installed.")
 
     sys.stdout = old_stdout
     assert len(mystdout.getvalue()) == 0, \
         "Unexpected output on import: %s" % mystdout.getvalue()
+
 
 # Manually tested:
 #   Uninstalled PygameSilent.
